bb4474f74267feac42cd1e397fa3eb1be9f67737
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2025-04-27 12:30:08 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -14,3 +14,4 @@ define('OAUTH_CLIENT_ID', 'cXVI6w5oZsO0Crj4qZNP0XVT2stLKKB6si2u0bqX');
 define('OAUTH_CLIENT_SECRET', 'KXxloxvACn43DxNaQTMchbamjobr1gHFUL58ML14UXkxyUDwnNOnXVSCCxBPtkmphf9mQUFsSdyJp1EhFNXXheBGOYklhR6RAed2iRkbL8d8DO9kn0mo9klzVwxKpzUM');
 define('OAUTH_REDIRECT_URI', SITE_URL . '/admin/oauth_callback.php');
 define('OAUTH_PROVIDER_URL', 'https://auth.wavehost.org/application/o/web/');
+define('ADMIN_EMAIL', 'admin@example.com'); // Replace with the actual admin email address
